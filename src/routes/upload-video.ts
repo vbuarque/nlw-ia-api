6995537c10e5bb0788de1223fb8202a9624e4c6c
vbuarque <viniciusbuarque17@gmail.com>
2023-09-14 19:28:43 -0300

    fix: corrigindo importação pasta tmp uploadDestination

--- src/routes/upload-video.ts
+++ src/routes/upload-video.ts
@@ -31,7 +31,7 @@ export async function uploadVideoRoute(app: FastifyInstance) {
 
         const fileBaseName = path.basename(data.filename, extension)
         const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`
-        const uploadDestination = path.resolve(__dirname, '../../tmp',  fileUploadName)
+        const uploadDestination = path.resolve(__dirname, '../tmp',  fileUploadName)
 
         await pump(data.file, fs.createWriteStream(uploadDestination))
 
